f629952a11b717bc2c32adb4e0cd84f2a65a077a
Sabine Scott <38441493+sabinescott@users.noreply.github.com>
2019-02-13 06:27:34 -0800

    Add files via upload

---
+++ models/Article.js
@@ -0,0 +1,37 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var ArticleSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+      type: String, 
+      required: true
+  },
+  link: {
+    type: String,
+    required: true
+  },
+  img: {
+    type: String,
+    required: true
+  },
+  sport: {
+    type:String,
+    required:true
+  },
+  saved: {
+    type: Boolean,
+    default: false
+  },
+  comment: {
+    type: Schema.Types.ObjectId,
+    ref: "Comment"
+  }
+});
+
+var Article = mongoose.model("Article", ArticleSchema);
+
+module.exports = Article;
